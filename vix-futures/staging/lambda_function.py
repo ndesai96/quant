91b62f02f2e62bbc7818fd3fba2f548c19d3024e
Neil Desai <ndesai1996@gmail.com>
2025-12-03 11:43:38 -0600

    add contango signal

--- vix-futures/staging/lambda_function.py
+++ vix-futures/staging/lambda_function.py
@@ -9,14 +9,29 @@ def lambda_handler(event, context):
     elif vix_spot < (vix_sma * 0.90):
         signal = "CRUSHED"
     
+    vix_3m_spot = get_spot('^VIX3M')
+    contango_spread = (vix_3m_spot - vix_spot) / vix_spot
+
+    if contango_spread >= 0.15:
+        contango_signal = "YELLOW (Contango - Extreme Complacency)"
+    elif contango_spread >= 0:
+        contango_signal ="GREEN (Contango - Normal Market)"
+    elif contango_spread > -0.05:
+        contango_signal = "ORANGE (Backwardation - Market Stress)"
+    else:
+        contango_signal = "RED (Deep Backwardation - Severe Stress/Panic)"
+
     return {
         'statusCode': 200,
         'body': {
             'vix': {
                 'spot': vix_spot,
                 'sma': vix_sma,
-                'diff_pct': ((vix_spot - vix_sma) / vix_sma) * 100,
-                'signal': signal
+                'diff_pct': '{:.2f}%'.format(((vix_spot - vix_sma) / vix_sma) * 100),
+                'signal': signal,
+                'vix_3m_spot': vix_3m_spot,
+                'contango_spread': '{:.2f}%'.format(contango_spread * 100),
+                'contango_signal': contango_signal
             }
         }
     }
@@ -27,3 +42,7 @@ def get_history(ticker, window=20) -> tuple[float, float]:
     spot = data['Close'].ffill().iloc[-1].item()
     sma = data['Close'].rolling(window=window).mean().iloc[-1].item()
     return spot, sma
+
+def get_spot(ticker) -> float:
+    data = yf.download(ticker, period="5d", interval="1d", progress=False, auto_adjust=True)
+    return data['Close'].ffill().iloc[-1].item()
