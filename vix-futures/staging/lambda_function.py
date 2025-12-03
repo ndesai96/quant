b9fb2ec2862316d8e793aac30378856e56774146
Neil Desai <ndesai1996@gmail.com>
2025-12-03 10:47:45 -0600

    fetch VIX3M and SKEW

--- vix-futures/staging/lambda_function.py
+++ vix-futures/staging/lambda_function.py
@@ -1,14 +1,17 @@
 import yfinance as yf
 
 def lambda_handler(event, context):
-    vix = yf.download("^VIX", period="5d", interval="1m", progress=False, auto_adjust=True)
+    tickers = ['^VIX', '^VIX3M', '^SKEW']
+    data = yf.download(tickers, period="5d", interval="1d", progress=False, auto_adjust=True)
+
+    # Forward fill to handle missing data for some tickers on the last day
+    closes = data['Close'].ffill()
 
     return {
         'statusCode': 200,
         'body': {
-            "vix": {
-                "close": vix["Close"].iloc[-1].item(),
-                "datetime": vix.index[-1].tz_convert('US/Central').isoformat(),
-            }
+            "vix": closes['^VIX'].iloc[-1].item(),
+            "vix3m": closes['^VIX3M'].iloc[-1].item(),
+            "skew": closes['^SKEW'].iloc[-1].item(),
         }
     }
