490d2f46bd87e6df16fbbd06401e43d0952d16c0
Neil Desai <ndesai1996@gmail.com>
2025-12-06 12:58:50 -0600

    add CORS headers

--- vix-futures/handlers/futures.py
+++ vix-futures/handlers/futures.py
@@ -42,6 +42,11 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET'
+        },
         'body': json.dumps(response)
     }
     
